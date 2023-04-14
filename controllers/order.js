7e0a2db97587b05070a12dd2deaeda64e18103e6
EnzoDamianAlderete <enzoalderete04@gmail.com>
2023-04-13 22:55:18 -0300

    se modifica controller de order

--- controllers/order.js
+++ controllers/order.js
@@ -20,30 +20,35 @@ export const getOrder = async (req, res) => {
 };
 
 export const createOrder = async (req, res) => {
-  const { name_user, price, number_table, userId, detalle_pedido } = req.body;
+  const { name_user, price, number_table, detalle_pedido, userId } = req.body;
 
   const user = await User.findById(userId);
+  // console.log({ name_user, price, number_table, detalle_pedido, userId });
 
-  if (!name_user || !price) {
-    return res.json({ mensaje: "Falta un campo por completar" });
+  //si se sca el price llega mas de 3 productos ... cuando se suma mas de uno tira el nan en el price total de la orden
+  if (!number_table || !name_user || !user || !price) {
+    return res.status(400).json({ mensaje: "Falta un campo por completar" });
   } else {
-    const newOrder = new Order({
-      name_user,
-      price,
-      number_table,
-      user: user._id,
-      detalle_pedido,
-    });
-
+    console.log({ detalle_pedido });
     try {
+      const newOrder = new Order({
+        name_user,
+        price,
+        number_table,
+        detalle_pedido,
+        user: user._id,
+      });
+
+      console.log({ newOrder: JSON.stringify(newOrder) });
+
       const savedOrder = await newOrder.save();
 
       user.orders = user.orders.concat(savedOrder._id);
       await user.save();
-
-      res.json({ mensaje: "Orden tomanda correctamente", savedOrder });
+      return res.json({ mensaje: "Orden tomanda correctamente", savedOrder });
     } catch (error) {
       console.error(error);
+      return res.status(500).json({ mensaje: "error interno" });
     }
   }
 };
@@ -65,6 +70,19 @@ export const deleteOrder = async (req, res) => {
   try {
     const order = await Order.findOneAndDelete({ _id: req.params.id });
     res.status(200).json({ mensaje: "Orden eliminada correctamente", order });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+export const confirmarOrder = async (req, res) => {
+  try {
+    const order = await Order.findOneAndUpdate(
+      { completed: req.params.completed },
+      req.body,
+      {}
+    );
+    res.status(200).json({ mensaje: "Orden confirmada correctamente", order });
   } catch (error) {
     res.status(500).json(error);
   }
